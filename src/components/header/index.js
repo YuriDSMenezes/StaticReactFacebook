81b380d263f75055877c64fbf693857bc14aec2b
Yuri <yuridasilvamenezes@gmail.com>
2019-12-11 07:00:38 +0000

    :rocket:

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './index.css'
 
 function Header() {
-  return (
-    <div className='header'>
-      <h1>GOOSTACK </h1>
-      <div className="profile">
-        <a href=''>Meu perfil</a>
-      </div>
-    </div>
-  )
+    const avatar = "https://api.adorable.io/avatars/30/user.png"
+
+    return (
+        <div className='header'>
+            <h1>GOOSTACK </h1>
+            <div className="profile">
+                <a href=''>Meu perfil</a>
+                <a href=''>
+                    <img src={avatar}></img>
+                </a>
+            </div>
+        </div>
+    )
 }
 export default Header;
